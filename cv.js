b860cf67b6fcdf4a17bd6ae47e539a57835afece
Smail93110 <smailouldbey93pro@gmail.com>
2022-10-31 22:57:18 +0100

    changement

---
+++ cv.js
@@ -0,0 +1,10 @@
+
+const loader = document.querySelector('.loader');
+
+
+window.addEventListener('load',  () => {
+   setTimeout(function () {
+      loader.classList.add('fondu-out');
+   }, 1000)
+})
+  
