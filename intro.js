18edc2a70c387fab98b2aaf7eb3571cbd5b13876
Smail93110 <smailouldbey93pro@gmail.com>
2022-11-01 22:42:12 +0100

    mise en place du Typewriter

---
+++ intro.js
@@ -0,0 +1,32 @@
+const txtAnim = document.querySelector('h1');
+
+new Typewriter(txtAnim,{
+    loop: true,
+    deletespeed: 10
+
+})
+
+.changeDelay(20)
+.typeString(' Smail Ould Bey')
+.pauseFor(300)
+.typeString('<strong>, Dev en </strong>')
+
+.typeString('<span style="color: red"> HTML</span> ! ')
+.pauseFor(2000)
+.deleteChars(7)
+
+
+
+.typeString('<span style="color: blue"> CSS</span> !')
+.pauseFor(2000)
+.deleteChars(5)
+
+.typeString('<span style="color: green"> PHP</span> !')
+.pauseFor(2000)
+.deleteChars(5)
+
+.typeString('<span style="color: #D26901"> PHPmyAdmin</span> !')
+.pauseFor(2000)
+.deleteChars(12)
+
+.start()
