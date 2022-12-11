f969cf9cf167164995819c15561e0eab2674af1c
Smail93110 <smailouldbey93pro@gmail.com>
2022-12-11 14:54:19 +0100

    mise a niveau du framwork

--- cv.js
+++ cv.js
@@ -1,5 +1,18 @@
 
 const loader = document.querySelector('.loader');
+const nav = document.querySelector('nav');
+
+
+window.addEventListener('load', () => {
+   setTimeout(function () {
+      loader.classList.add('nav');
+   }, 1000)
+})
+
+
+
+
+
 
 
 window.addEventListener('load', () => {
